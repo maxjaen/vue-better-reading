9d37392e6a93c18fcdb23ba347d917c0b67e6f48
Max Jänicke <max.jaenicke@gmx.de>
2020-09-04 08:30:16 +0200

    add home component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
+body,
+html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  min-height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: inherit;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <div class="input">
+      <textarea v-model="insertedText" placeholder="Paste your text"></textarea>
+      <div>
+        <button v-on:click="processWords()">Display</button>
+      </div>
+      <p style="white-space: pre-line;">{{ currentDisplayedWord }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    insertedText: String,
+    currentDisplayedWord: String,
+  },
+  data: function() {
+    return {
+      text: this.insertedText,
+      word: this.currentDisplayedWord,
+    };
+  },
+  methods: {
+    processWords: async function() {
+      const wordsFromInsertion = this.insertedText.split(' ');
+
+      while (wordsFromInsertion.length !== 0) {
+        this.currentDisplayedWord = wordsFromInsertion.shift();
+        await this.wait(300);
+      }
+    },
+    wait: async function(milliseconds) {
+      return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  height: 100vh;
+  min-width: 300px;
+  background-color: #7e9389;
+  padding: 70px;
+}
+textarea {
+  height: 250px;
+  width: -moz-available;
+}
+</style>
